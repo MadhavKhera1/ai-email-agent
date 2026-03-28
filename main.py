97763f6f96227ed654404fb22c1d286daaa4a3f1
MadhavKhera1 <madhavkhera868@gmail.com>
2026-03-28 11:39:15 +0530

    Initial project setup: folder structure, config, logger, main entry point

---
+++ main.py
@@ -0,0 +1,9 @@
+from utils.logger import get_logger
+
+logger = get_logger("MAIN")
+
+def main():
+    logger.info(" Email Agent Started (Windows Setup Done)")
+
+if __name__ == "__main__":
+    main()
